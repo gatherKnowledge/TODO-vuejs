4978c021b6a29a6a05f8bfaac56ef34e633dd1c8
luke.moon <seonghyun1143@sint.kr>
2019-10-25 18:19:39 +0900

    TEST

---
+++ data.js
@@ -0,0 +1,19 @@
+export default todoList;
+
+const todoList = [
+  {
+    index: 0,
+    title: '집에가기',
+		isDone: false
+	},
+	{
+    index: 1,
+    title: '밥먹기',
+		isDone: false
+	},
+	{
+    index: 2,
+    title: '샤워하기',
+		isDone: true
+	}
+]
---
+++ middleware/default.js
@@ -0,0 +1,5 @@
+export default function({ store, redirect, error, app }) {
+  
+}
+// 특정 url에 접속할 때마다 동작하는 middleware 필요
+// TODO: auth 테스트
