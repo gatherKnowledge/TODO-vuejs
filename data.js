f8ebc38a3a9d15c6d10672d5c45ba21ffb0a76f4
luke_home <luke.moon@pinplay.io>
2019-10-27 22:33:54 +0900

    기본기능

--- data.js
+++ data.js
@@ -1,19 +1,18 @@
-export default todoList;
-
 const todoList = [
   {
     index: 0,
-    title: '집에가기',
-		isDone: false
-	},
-	{
+    text: '집에가기',
+      isDone: false
+    },
+    {
     index: 1,
-    title: '밥먹기',
-		isDone: false
-	},
-	{
+    text: '밥먹기',
+      isDone: false
+    },
+    {
     index: 2,
-    title: '샤워하기',
-		isDone: true
-	}
-]
+    text: '샤워하기',
+      isDone: true
+  }
+];
+export default todoList;
---
+++ store/todos.js
@@ -0,0 +1,20 @@
+import todoList from '../data';
+export const state = () => ({
+  list: todoList
+})
+
+export const mutations = {
+  add (state, text) {
+    state.list.push({
+      text,
+      done: false
+
+    })
+  },
+  remove (state, { todo }) {
+    state.list.splice(state.list.indexOf(todo), 1)
+  },
+  toggle (state, todo) {
+    todo.done = !todo.done
+  }
+}
